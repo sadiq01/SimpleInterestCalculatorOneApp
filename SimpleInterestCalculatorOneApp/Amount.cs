07a0798140c431e9ee0204fb1c0e4641eacd3067
sadiq01 <sadiqur01@gmail.com>
2015-05-02 11:41:31 +0600

    Intial file for simple interest Calculator

---
+++ SimpleInterestCalculatorOneApp/Amount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInterestCalculatorOneApp
+{
+    class Amount
+    {
+        public double principleAmount, annualInterestRate, timePeriod;
+        public double totalAmount;
+    public double GetTotalAmount()
+    {
+        return principleAmount*(Math.Pow((1 + annualInterestRate),timePeriod));
+    }
+    }
+    
+}
---
+++ SimpleInterestCalculatorOneApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleInterestCalculatorOneApp
+{
+    public partial class SimpleInterestCalculatorUi : Form
+    {
+        public SimpleInterestCalculatorUi()
+        {
+            InitializeComponent();
+        }
+        Amount bAmount=new Amount();
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            bAmount.principleAmount = Convert.ToDouble(principalAmountTextBox.Text);
+            bAmount.annualInterestRate = (Convert.ToDouble(annualInterestRateTextBox.Text))/100;
+            bAmount.timePeriod = Convert.ToDouble(timePeriodTextBox.Text);
+            bAmount.totalAmount = bAmount.GetTotalAmount();
+            totalAmountTextBox.Text = bAmount.totalAmount.ToString();
+        }
+    }
+}
